38c78beba472ed72af466f554e68e4b1d7a3986c
seetide <seetide@gmail.com>
2019-12-04 17:01:55 -0500

    messy test

--- src/main/java/com/haitao/msscbeerservice/web/service/BeerServiceImpl.java
+++ src/main/java/com/haitao/msscbeerservice/web/service/BeerServiceImpl.java
@@ -1,9 +1,21 @@
 package com.haitao.msscbeerservice.web.service;
 
+import com.haitao.msscbeerservice.web.model.BeerDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return null;
+    }
+
+    @Override
+    public void updateBeer(UUID bearId, BeerDto beerDto) {
+
+    }
 }
---
+++ src/test/java/com/haitao/msscbeerservice/web/controller/BeerControllerTest.java
@@ -0,0 +1,34 @@
+package com.haitao.msscbeerservice.web.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BeerController.class)
+class BeerControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+
+    @Test
+    void getBeerById() throws Exception {
+        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void saveNewBeer() {
+    }
+
+    @Test
+    void updateBeer() {
+    }
+}
