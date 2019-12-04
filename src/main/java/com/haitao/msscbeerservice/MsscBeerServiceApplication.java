9a35eb106e1d42a6edf48f62ee8ea5b5c8446fa7
seetide <seetide@gmail.com>
2019-12-04 14:21:15 -0500

    Inital project add. Close #1

---
+++ src/main/java/com/haitao/msscbeerservice/MsscBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.haitao.msscbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscBeerServiceApplication.class, args);
+    }
+
+}
