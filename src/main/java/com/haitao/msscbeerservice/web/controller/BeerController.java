9a096a889e5759480966b884907017216e5a853b
seetide <seetide@gmail.com>
2019-12-04 16:23:37 -0500

    add beer controller Close #3

---
+++ src/main/java/com/haitao/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,42 @@
+package com.haitao.msscbeerservice.web.controller;
+
+import com.haitao.msscbeerservice.web.model.BeerDto;
+import com.haitao.msscbeerservice.web.service.BeerService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+    private final BeerService beerService;
+    public BeerController(BeerService beerService) {
+        this.beerService = beerService;
+    }
+
+    @GetMapping({"/{beerId}"})
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable UUID beerId){
+        //todo impl
+        return new ResponseEntity<>(BeerDto.builder().id(beerId).build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
+        BeerDto savedDto = beerService.saveNewBeer(beerDto);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/beer/" + savedDto.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeer(@RequestBody BeerDto  beerDto, @PathVariable UUID bearId) {
+        //todo  implement
+        beerService.updateBeer(bearId, beerDto);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
---
+++ src/main/java/com/haitao/msscbeerservice/web/service/BeerService.java
@@ -0,0 +1,11 @@
+package com.haitao.msscbeerservice.web.service;
+
+import com.haitao.msscbeerservice.web.model.BeerDto;
+
+import java.util.UUID;
+
+public interface BeerService {
+    BeerDto saveNewBeer(BeerDto beerDto);
+
+    void updateBeer(UUID bearId, BeerDto beerDto);
+}
